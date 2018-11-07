6e45450955945c54d238d701bf35dd3f29fe02e6
sharhs <41955276+sharhs@users.noreply.github.com>
2018-11-07 11:39:27 +0300

    Update column_to_string.sql

--- column_to_string.sql
+++ column_to_string.sql
@@ -1,4 +1,3 @@
-
 |column|
 --------
 |  1   |
@@ -6,11 +5,9 @@
 |  5   |
 |  9   |
 
-
 declare @results varchar(500)
-
 select @results = coalesce(@results + ',', '') +  convert(varchar(12),col)
-from t
+  from t
 order by col
 
 select @results as results
@@ -19,14 +16,7 @@ select @results as results
 -----------
 | 1,3,5,9 |
 
-
-
-
-
 ---
-
-
-
 SELECT (email + ';') AS 'text()' FROM dbo.person_mailing_list
 WHERE do_not_send = 0
 FOR Xml PATH ('')
