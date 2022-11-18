e6b1390ca2ac3c97279d5208435e359fb123181d
sharhs <41955276+sharhs@users.noreply.github.com>
2022-11-18 18:50:34 +0300

    Update query_profiles.sql

--- query_profiles.sql
+++ query_profiles.sql
@@ -15,3 +15,22 @@ select deqp.session_id
       ,deqp.actual_read_row_count
 from   sys.dm_exec_query_profiles as deqp
 where  deqp.session_id = 
+
+
+
+select deqp.session_id
+      ,deqp.node_id
+	  ,deqp.physical_operator_name
+      ,sum(deqp.row_count)  row_count
+      ,sum(deqp.estimate_row_count) estimate_row_count
+      ,sum(deqp.scan_count) scan_count
+      ,sum(deqp.logical_read_count ) logical_read_count
+      ,sum(deqp.physical_read_count) physical_read_count
+      ,sum(deqp.read_ahead_count) read_ahead_count
+      ,sum(deqp.write_page_count) write_page_count
+      ,sum(deqp.estimated_read_row_count) estimated_read_row_count
+      ,sum(deqp.actual_read_row_count) actual_read_row_count
+from   sys.dm_exec_query_profiles as deqp
+where  deqp.session_id = 870
+GROUP BY deqp.session_id, deqp.node_id, deqp.physical_operator_name
+ORDER BY deqp.node_id ASC
