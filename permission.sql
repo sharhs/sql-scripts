b17965b94817a0756a8f3236ebaf4ae3589b9433
sharhs <41955276+sharhs@users.noreply.github.com>
2025-04-07 10:57:54 +0300

    Update permission.sql

--- permission.sql
+++ permission.sql
@@ -18,3 +18,19 @@ SELECT	spri.name
 		ON	spr.grantee_principal_id = spri.principal_id
 WHERE spri.name like '%%'
 ORDER BY OBJECT_NAME(spr.major_id)
+
+
+SELECT spr.state_desc collate SQL_Latin1_General_CP1251_CI_AS + '' + spr.permission_name +
+isnull( ' on ' + OBJECT_NAME(spr.major_id) ,''
+)+ ' to ' + 	spri.name,spri.name
+			,OBJECT_NAME(spr.major_id)
+			,spr.*
+	FROM sys.database_permissions spr 
+	JOIN sys.database_principals spri
+		ON	spr.grantee_principal_id = spri.principal_id
+WHERE spri.name like '%%'
+and permission_name <> 'CONNECT'
+ORDER BY OBJECT_NAME(spr.major_id)
+
+
+
